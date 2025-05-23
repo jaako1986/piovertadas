4c77a5fd9e525916b666a41505637d57b3cbc674
jaako1986 <diearav@gmail.com>
2025-05-23 20:21:19 -0300

    fix: puerto para Render

--- app.py
+++ app.py
@@ -6,5 +6,8 @@ app = Flask(__name__)
 def inicio():
     return render_template('index.html')
 
-if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
