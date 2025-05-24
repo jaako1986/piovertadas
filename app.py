2d481daca97e0689937bea9ddf38cd9d55d5742c
jaako1986 <diearav@gmail.com>
2025-05-24 20:11:16 -0300

    fix para port 0 0 0 0

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -6,14 +7,12 @@ app = Flask(__name__)
 def inicio():
     return render_template('index.html')
 
-if __name__ == '__main__':
-    app.run(debug=True)
-
 @app.route("/galeria")
 def galeria():
     ruta = os.path.join(app.static_folder, "fotos")
     imagenes = [f"fotos/{img}" for img in os.listdir(ruta) if img.endswith((".jpg", ".png", ".jpeg", ".webp"))]
     return render_template("galeria.html", imagenes=imagenes)
+
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(debug=True, host="0.0.0.0", port=port)
