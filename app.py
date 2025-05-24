4ee1927db8d66f7854347b5b3089c3715a236c8c
jaako1986 <diearav@gmail.com>
2025-05-24 20:05:39 -0300

    fix para port 0 0 0 0

--- app.py
+++ app.py
@@ -14,3 +14,6 @@ def galeria():
     ruta = os.path.join(app.static_folder, "fotos")
     imagenes = [f"fotos/{img}" for img in os.listdir(ruta) if img.endswith((".jpg", ".png", ".jpeg", ".webp"))]
     return render_template("galeria.html", imagenes=imagenes)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host="0.0.0.0", port=port)
