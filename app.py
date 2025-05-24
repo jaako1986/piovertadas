64362e94f1693ddb2ddc0610c0a70e3eda02e24f
jaako1986 <diearav@gmail.com>
2025-05-24 10:05:40 -0300

    ajustes varios

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
-from flask import Flask, render_template
 import os
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
 @app.route("/")
 def inicio():
-    return render_template("index.html")
+    ruta = os.path.join(app.static_folder, "fotos")
+    imagenes = [f"fotos/{img}" for img in os.listdir(ruta) if img.endswith((".jpg", ".png", ".jpeg", ".webp"))]
+    return render_template("index.html", imagenes=imagenes)
 
-# Esto es lo que necesitás para que funcione en Render
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
